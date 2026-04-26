606c0a19b6fab5ba2a62cd8af0a524403304c92f
Lizbeth.B <al222835@alumnos.uacj.mx>
2026-04-25 19:08:57 -0600

    feat: sidebar component

--- frontend/src/components/common/Switch.jsx
+++ frontend/src/components/common/Switch.jsx
@@ -3,7 +3,7 @@ export function Switch({ checked = false, onChange }) {
     <button
       onClick={() => onChange(!checked)}
       className={`
-        relative w-12 h-6 rounded-full
+        relative shrink-0 w-11 h-6 rounded-full
         transition-colors duration-(--transition-fast)
         ${checked ? "bg-aux" : "bg-gray-200"}
       `}
@@ -11,7 +11,7 @@ export function Switch({ checked = false, onChange }) {
       <span className={`
         absolute top-1 w-4 h-4 rounded-full
         transition-all duration-(--transition-fast)
-        ${checked ? "left-7 bg-bg-light" : "left-1 bg-gray-400"}
+        ${checked ? "left-6 bg-bg-light" : "left-1 bg-gray-400"}
       `} />
     </button>
   )
