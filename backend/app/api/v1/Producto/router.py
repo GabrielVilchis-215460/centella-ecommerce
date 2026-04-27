11b36bad9a8dd392e9913ffdc495182a9740a620
GabrielVilchis-215460 <al215460@alumnos.uacj.mx>
2026-04-26 18:36:20 -0600

    feat: add protections for client, admin and emprendedora endpoints (2/2)

--- backend/app/api/v1/Producto/router.py
+++ backend/app/api/v1/Producto/router.py
@@ -3,6 +3,7 @@ from sqlalchemy.orm import Session
 from app.core.database import get_db
 from .schemas import ProductoCreate, ProductoUpdate, ProductoRead
 from . import service
+from app.core.deps import require_emprendedora_or_admin
 
 router = APIRouter(prefix="/productos", tags=["Productos"])
 
@@ -17,16 +18,16 @@ def obtener(id_producto: int, db: Session = Depends(get_db)):
     return service.get_by_id(db, id_producto)
 
 
-@router.post("/", response_model=ProductoRead, status_code=status.HTTP_201_CREATED)
+@router.post("/", response_model=ProductoRead, status_code=status.HTTP_201_CREATED, dependencies=[Depends(require_emprendedora_or_admin)])
 def crear(data: ProductoCreate, db: Session = Depends(get_db)):
     return service.create(db, data)
 
 
-@router.put("/{id_producto}", response_model=ProductoRead)
+@router.put("/{id_producto}", response_model=ProductoRead, dependencies=[Depends(require_emprendedora_or_admin)])
 def actualizar(id_producto: int, data: ProductoUpdate, db: Session = Depends(get_db)):
     return service.update(db, id_producto, data)
 
 
-@router.delete("/{id_producto}", status_code=status.HTTP_204_NO_CONTENT)
+@router.delete("/{id_producto}", status_code=status.HTTP_204_NO_CONTENT, dependencies=[Depends(require_emprendedora_or_admin)])
 def eliminar(id_producto: int, db: Session = Depends(get_db)):
     service.delete(db, id_producto)
